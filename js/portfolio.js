2a3a82c42b52e1cb43028be6401be909ce17ef16
Alexandra Demetriades <amdemetriades@gmail.com>
2023-10-18 14:27:09 +0100

    Carousel for portfolio built, partial js for carousel, added images.

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,20 +1,46 @@
-const submit = document.getElementById('submit');
+const track = document.querySelector('.carousel_track');
+const slides = Array.from(track.children);
+const nextButton = document.querySelector('.carousel_button--right');
+const prevButton = document.querySelector('.carousel_button--left');
+const dotsNav = document.querySelector('.carousel_nav');
+const dots = Array.from(dotsNav.children);
 
-submit.addEventListener('click', validate);
-
-function validate(e) {
-    e.preventDefault();
-
-    const name = document.getElementById('fullName')
-    let valid = true;
-
-    if (!.fullName.value) {
-
-    }
-
-}
+const slideWidth = slides[0].getBoundingClientRect().width;
+// const slideWidth = slideSize.width;
 
 
 
-    
-}
+// arrange slides next to each other - currently on top of each other
+
+// slides[0].style.left = * 0 + 'px';
+// slides[1].style.left = slideWidth + 'px';
+// slides[2].style.left = slideWidth * 2 + 'px';
+
+const setSlidePosition = (function(slide, index) {
+    slide.style.left = slideWidth * index +'px'
+});
+
+// slides.forEach(function (slide, index) {
+//     slide.style.left = slideWidth * index + 'px';
+// })   ---> added this into a function stored in a variable above.
+
+slides.forEach(setSlidePosition) //used variable function here
+
+
+// click left, move
+nextButton.addEventListener('click', function(e){
+    const currentSlide = track.querySelector('.current-slide');
+    const nextSlide = currentSlide.nextElementSibling;  // move to next slide
+    const amountToMove = nextSlide.style.left //how far the element must move
+
+    track.style.transform = 'translateX(- " + amountToMove + ")';
+
+})
+
+
+
+// click right, move
+// click nav dots change to relevant slide
+
+
+// console.log(slideSize);http://127.0.0.1:3000/images/luna.png
