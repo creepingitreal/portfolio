b08296ed436c68364c87905617b4855450548c03
Alexandra Demetriades <amdemetriades@gmail.com>
2023-10-18 11:54:09 +0100

    Added most of the html/css carousel for projects/work section. still needs JS and .json to be added to make functional

---
+++ js/portfolio.js
@@ -0,0 +1,20 @@
+const submit = document.getElementById('submit');
+
+submit.addEventListener('click', validate);
+
+function validate(e) {
+    e.preventDefault();
+
+    const name = document.getElementById('fullName')
+    let valid = true;
+
+    if (!.fullName.value) {
+
+    }
+
+}
+
+
+
+    
+}
